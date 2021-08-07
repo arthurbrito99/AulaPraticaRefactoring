955c681c3f04f0657b083ebae404e18c03707514
Arthur de Brito <arthur.brito1999@outlook.com>
2021-08-07 17:54:56 +0000

    Commit 18

--- TextStatement.java
+++ TextStatement.java
@@ -2,43 +2,27 @@ import java.util.Enumeration;
 
 public class TextStatement extends Statement {
 
-  public String value(Customer aCustomer) {
-    Enumeration rentals = aCustomer.getRentals();
-    String result = getInitialString(aCustomer);
-
-    while (rentals.hasMoreElements()) {
-      Rental each = (Rental) rentals.nextElement();
-      //show figures for this rental
-      result += getFiguresString(each);
-    }
-
-    //add footer lines
-    result += getOwedString(aCustomer);
-    result += getEarnedString(aCustomer);
-    return result;
-  }
-
-  private String getInitialString(Customer aCustomer) {
+  protected String getInitialString(Customer aCustomer) {
     String result = "Rental Record for " + aCustomer.getName() +
     "\n";
 
     return result;
   }
 
-  private String getFiguresString(Rental aRental) {
+  protected String getFiguresString(Rental aRental) {
     String result = "\t" + aRental.getMovie().getTitle()+ "\t" +
         String.valueOf(aRental.getCharge()) + "\n";
 
     return result;
   }
 
-  private String getOwedString(Customer aCustomer) {
+  protected String getOwedString(Customer aCustomer) {
     String result = "Amount owed is " +
       String.valueOf(aCustomer.getTotalCharge()) + "\n";
     return result;
   }
 
-  private String getEarnedString(Customer aCustomer) {
+  protected String getEarnedString(Customer aCustomer) {
     String result = "You earned " +
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
       " frequent renter points";
